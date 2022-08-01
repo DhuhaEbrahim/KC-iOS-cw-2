828c860dd3fb6d7459ad94e4dea04f17fb11329d
DhuhaEbrahim <109903315+DhuhaEbrahim@users.noreply.github.com>
2022-08-01 19:42:52 +0300

    done

---
+++ day2Classwork/day2Classwork/day2ClassworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  day2ClassworkApp.swift
+//  day2Classwork
+//
+//  Created by dhuha kaweyani on 30/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct day2ClassworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
